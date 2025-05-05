03a93e7fb05916439c10bf65ee3d476f6c97af0a
Manuel Smulovitz <49439375@est.ort.edu.ar>
2025-05-05 10:29:04 -0300

    todo

---
+++ Assets/scripts/uimanager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class uimanager : MonoBehaviour
+{
+    public TextMeshProUGUI resultado;
+    public TMP_InputField IngresarClave;
+    string claveCorrecta = "algo";
+    string claveIng;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        resultado.text = "";   
+    }
+    public void checkClave()
+    {
+        claveIng = IngresarClave.text;
+        if(claveIng == claveCorrecta)
+        {
+            resultado.text = "Clave correcta";
+        }
+        else
+        {
+            resultado.text = "Clave Incorrecta";
+        }
+    }
+
+}
